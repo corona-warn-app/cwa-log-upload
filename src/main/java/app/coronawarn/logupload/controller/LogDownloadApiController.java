9ab9eb8941ed94df889e6104365342eac0735872
Felix Dittrich <31076102+f11h@users.noreply.github.com>
2022-04-27 13:05:06 +0200

    Chore: Update H2 and Liquibase (#32)
    * Update H2 and Liquibase
    
    * Update CommonsIO
    Remove Guava

--- src/main/java/app/coronawarn/logupload/controller/LogDownloadApiController.java
+++ src/main/java/app/coronawarn/logupload/controller/LogDownloadApiController.java
@@ -22,12 +22,12 @@
 package app.coronawarn.logupload.controller;
 
 import app.coronawarn.logupload.service.FileStorageService;
-import com.google.common.net.HttpHeaders;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
